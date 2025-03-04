56b0f4a134020e65e151d1355a443b3a8e3e6179
SivaSree-01 <sivasree2909@gmail.com>
2025-03-04 17:42:31 +0530

    Project

---
+++ pro.js
@@ -0,0 +1,21 @@
+let salary = 0;
+let balance = 0;
+function setSalary() {
+    salary = Number(document.getElementById('salaryInput').value);
+    balance = salary;
+    document.getElementById('salary').innerText = salary;
+    document.getElementById('balance').innerText = balance;
+    document.getElementById('salaryInput').value = '';
+}
+function addTransaction() {
+    const desc = document.getElementById('desc').value;
+    const amount = Number(document.getElementById('amount').value);
+    if (desc.trim() === '' || isNaN(amount) || amount <= 0 || amount > balance) return;
+    balance -= amount;
+    document.getElementById('balance').innerText = balance;
+    const li = document.createElement('li');
+    li.innerHTML = `${desc} <span class="expense">$${amount}</span>`;
+    document.getElementById('transactions').appendChild(li);
+    document.getElementById('desc').value = '';
+    document.getElementById('amount').value = '';
+}
